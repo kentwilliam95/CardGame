4ae89408a68c8ce60526d11020f0af030356477b
kent william <Kentwilliam95@gmail.com>
2023-03-03 09:03:34 +0700

    Add functionality to show and hide result controller

--- Assets/Scripts/User IInterface/ResultController.cs
+++ Assets/Scripts/User IInterface/ResultController.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 namespace Pusoy
 {
     public class ResultController : MonoBehaviour
     {
-       public void Restart()
-       {
-        
-       }
+        [SerializeField] private TextMeshProUGUI _winnerText;
+        [SerializeField] private CanvasGroup _canvasGroup;
+        public void Show(string text)
+        {
+            _winnerText.text = $"Winner: {text}";
+            _canvasGroup.alpha = 1;
+            _canvasGroup.interactable = true;
+            _canvasGroup.blocksRaycasts = true;
+        }
+
+        public void Hide()
+        {
+            _canvasGroup.alpha = 0;
+            _canvasGroup.interactable = false;
+            _canvasGroup.blocksRaycasts = false;
+        }
     }
 }
